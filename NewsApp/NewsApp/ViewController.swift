6bbc6ff8648fe5aa9747e699645b34d7f829126c
Arundas MK <mkarundas@gmail.com>
2023-01-07 12:56:40 +0530

    project created

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Arundas MK on 07/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
